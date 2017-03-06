9fa0770bcc0106763d8548400aa9936c3871fda8
dark1618 <dark1618@gmail.com>
2017-03-06 17:46:50 +0100

    Felületterv
    A felület megtervezése (1. lépés)

---
+++ Viragkereskedes/wndFoablak.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Viragkereskedes
+{
+    /// <summary>
+    /// Interaction logic for wndFoablak.xaml
+    /// </summary>
+    public partial class wndFoablak : Window
+    {
+        public wndFoablak()
+        {
+            InitializeComponent();
+        }
+    }
+}
